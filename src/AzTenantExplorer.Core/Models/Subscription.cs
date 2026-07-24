c7af863e44c7007bcdbb1544a7c6919035ece0f2
Alexandru Bunea <alexandrubunea@Alexandrus-MacBook-Air.local>
2026-07-24 23:16:57 +0300

    feat: add Subscription model.

---
+++ src/AzTenantExplorer.Core/Models/Subscription.cs
@@ -0,0 +1,34 @@
+namespace AzTenantExplorer.Core.Models;
+
+public enum BillingPlatform
+{
+    Unknown,
+    MOSP,
+    MCA
+}
+
+public record Subscription
+(
+    string Id,
+    string SubscriptionGUID,
+    string DisplayName,
+    string State,
+    string OfferId,
+    string TenantGuid,
+    string SpendingLimit,
+
+    string? BillingAccountId,
+    string? BillingProfileId,
+    string? InvoiceSectionId
+)
+{
+    public BillingPlatform GetBillingPlatform =>
+        OfferId.EndsWith("P", StringComparison.OrdinalIgnoreCase) ? BillingPlatform.MOSP :
+        OfferId.EndsWith("G", StringComparison.OrdinalIgnoreCase) ? BillingPlatform.MCA :
+        BillingPlatform.Unknown;
+
+    public bool IsDevTest =>
+        OfferId == "MS-AZR-0148G" ||
+        OfferId == "MS-AZR-0148P" ||
+        OfferId == "MS-AZR-0023P";
+}
